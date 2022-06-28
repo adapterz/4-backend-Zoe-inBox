b375a5296b868bee018252256644baa6566d5702
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-06-28 22:26:30 +0900

    Chore : 실행 파일 명 변경 - app.java
    통일성있게 main 실행 파일을 app.java 로 이름 변경

--- src/main/java/inbox/inbox/app.java
+++ src/main/java/inbox/inbox/app.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InboxApplication {
+public class app {
 
     public static void main(String[] args) {
-        SpringApplication.run(InboxApplication.class, args);
+        SpringApplication.run(app.class, args);
     }
 
 }
