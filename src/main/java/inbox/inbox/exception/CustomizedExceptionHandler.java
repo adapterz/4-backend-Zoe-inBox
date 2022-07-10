00f46ba9251318adcfb162944e4fa5d6644d4294
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-07-10 17:58:19 +0900

    Feat : 예외 핸들링 클래스
    의도한 예외나 의도하지 않은 예외나 모두 이 클래스에서 관리
    AOP 개념을 적용해서 해당 클래스에서 '예외'라는 aspect 처리

---
+++ src/main/java/inbox/inbox/exception/CustomizedExceptionHandler.java
@@ -0,0 +1,40 @@
+package inbox.inbox.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+// 예외 핸들링
+@RestControllerAdvice
+public class CustomizedExceptionHandler extends ResponseEntityExceptionHandler {
+
+    // 예상치 못한 런타임 에러
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Object> handleUnexpectedException() {
+        ExceptionMessage exceptionMessage = new ExceptionMessage(
+            Timestamp.valueOf(LocalDateTime.now()), "unexpected_error");
+        return new ResponseEntity<Object>(exceptionMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    // 유효하지 않은 param
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleParamConstraintViolationException() {
+        ExceptionMessage exceptionMessage = new ExceptionMessage(
+            Timestamp.valueOf(LocalDateTime.now()), "invalid_request");
+        return new ResponseEntity<Object>(exceptionMessage, HttpStatus.BAD_REQUEST);
+    }
+
+    // 포트폴리오 range 의 조회 조건을 모두 off 했을 때
+    @ExceptionHandler(PortfolioConflictException.class)
+    public ResponseEntity<Object> handlePortfolioConflictException() {
+        ExceptionMessage exceptionMessage = new ExceptionMessage(
+            Timestamp.valueOf(LocalDateTime.now()), "at_least_one_should_be_on");
+        return new ResponseEntity<Object>(exceptionMessage, HttpStatus.CONFLICT);
+    }
+}
