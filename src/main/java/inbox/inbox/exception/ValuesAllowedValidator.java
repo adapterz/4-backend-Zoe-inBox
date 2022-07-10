7b3a4d82a106f925f0dd5c3dea8ff036e672b7c0
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-07-10 18:05:25 +0900

    Feat : param 유효성 검사를 위한 Validator
    포트폴리오 조회조건 변경할 API 유효성 검사

---
+++ src/main/java/inbox/inbox/exception/ValuesAllowedValidator.java
@@ -0,0 +1,29 @@
+package inbox.inbox.exception;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+
+// 유효성 검사 validator
+public class ValuesAllowedValidator implements ConstraintValidator<ValuesAllowed, String> {
+
+    private List<String> expectedValues;
+    private String returnMessage;
+
+    @Override
+    public void initialize(ValuesAllowed requiredIfChecked) {
+        expectedValues = Arrays.asList(requiredIfChecked.values());
+        returnMessage = requiredIfChecked.message();
+    }
+
+    @Override
+    public boolean isValid(String inputParam, ConstraintValidatorContext context) {
+        boolean valid = expectedValues.contains(inputParam);
+        if (!valid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(returnMessage).addConstraintViolation();
+        }
+        return valid;
+    }
+}
