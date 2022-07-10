6d1dc5eb91ec3278f9dfbe5a6197065281cc0b88
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-07-10 19:43:52 +0900

    Test : jenkins 서버에서 lombok 작동하는지 확인
    test

--- src/main/java/inbox/inbox/exception/ExceptionMessage.java
+++ src/main/java/inbox/inbox/exception/ExceptionMessage.java
@@ -1,11 +1,8 @@
 package inbox.inbox.exception;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.sql.Timestamp;
 
 // 예외 메세지 클래스
 @Data
