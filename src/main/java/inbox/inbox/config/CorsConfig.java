7f35a9ed6a92d743188bfb44f6896729971e47fb
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-07-18 16:45:20 +0900

    Chore : 사용안하는 import 구문, 어노테이션 삭제

--- src/main/java/inbox/inbox/config/CorsConfig.java
+++ src/main/java/inbox/inbox/config/CorsConfig.java
@@ -4,13 +4,11 @@ import static inbox.inbox.utils.ConstantManager.FRONT_URL;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // cors config
 @Configuration
-@Component
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
