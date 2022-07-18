6bed66b7d542eb1e551cfc3682bc7fcd78ba47a6
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-07-18 12:37:50 +0900

    Refactor : cors 에 의한 요청 거부 해결을 위한 수정
    preflight 요청 받아들이기 위해 cors config bean 등록

--- src/main/java/inbox/inbox/config/CorsConfig.java
+++ src/main/java/inbox/inbox/config/CorsConfig.java
@@ -3,11 +3,13 @@ package inbox.inbox.config;
 import static inbox.inbox.utils.ConstantManager.FRONT_URL;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // cors config
 @Configuration
+@Component
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
