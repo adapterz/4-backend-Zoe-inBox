b1424c11f9e929b685b438b3629214278b7e1cdd
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-07-18 12:15:01 +0900

    Refactor : cors 에 의한 요청 거부 해결을 위한 수정
    uploadPortfolio 요청만 cors 에 의해 거부되는 점 해결을 위한 테스트
    허용 http method 에 options 추가 및 자잘한 수정

--- src/main/java/inbox/inbox/config/CorsConfig.java
+++ src/main/java/inbox/inbox/config/CorsConfig.java
@@ -3,7 +3,6 @@ package inbox.inbox.config;
 import static inbox.inbox.utils.ConstantManager.FRONT_URL;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,8 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins(FRONT_URL)
-            .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
-                HttpMethod.PATCH.name(), HttpMethod.OPTIONS.name())
+            .allowedMethods("*")
             .allowCredentials(true).allowedHeaders("*").maxAge(86400);
     }
 }
